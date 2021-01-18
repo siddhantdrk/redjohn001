4dd796bbb9f3efb56afc27d30db8fc13346c8421
Siddhant Khobragade <siddhantdrk@gmail.com>
2021-01-18 22:33:34 +0530

    minor issues fixed

--- app/src/main/java/com/example/redjohn001/MainActivity.kt
+++ app/src/main/java/com/example/redjohn001/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_invite)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container_main, SchedulesFragment()).commit()
 //        val layout =
 //            (tabLayout.getChildAt(0) as LinearLayout).getChildAt(YOUR_TAB_NUMBER) as LinearLayout
 //        val layoutParams =
